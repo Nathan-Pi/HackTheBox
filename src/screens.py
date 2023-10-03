a6787f03560ce0f3d3822db0b48d1dea362b910b
Ethan McIntosh <128169416+emcint@users.noreply.github.com>
2023-10-03 16:42:32 +0100

    added method to append to leaderboard

--- src/screens.py
+++ src/screens.py
@@ -38,6 +38,18 @@ class Screen:
         else:
             return input(self.displayText)
 
+    def restartOrQuit(self):
+        while True:
+            choice = int(input("1 to restart, or 2 to quit: "))
+            if choice == 1:
+                name = str(input("Enter a name: "))
+                return name
+            elif choice == 2:
+                print("Quitting...")
+                quit()
+            else:
+                print("Invalid Input")
+
 
 class startScreen(Screen):
 
@@ -69,3 +81,109 @@ class startScreen(Screen):
                 quit()
             else:
                 print("Invalid Input")
+
+
+class loseScreen(Screen):
+    
+    def display(self):
+
+        os.system('cls')
+        print("""
+ __     __           _               _     _ 
+ \ \   / /          | |             | |   | |
+  \ \_/ /__  _   _  | |     ___  ___| |_  | |
+   \   / _ \| | | | | |    / _ \/ __| __| | |
+    | | (_) | |_| | | |___| (_) \__ \ |_  |_|
+    |_|\___/ \__,_| |______\___/|___/\__| (_)
+              """)
+
+        print("""
+                            ,--.
+                           K,   }
+                          /  ~Y`
+                     ,   /   /
+                    {_'-K.__/
+                      `/-.__L._
+                      /  ' /`\_}
+                     /  ' /
+             ____   /  ' /
+      ,-'~~~~    ~~/  ' /_
+    ,'             ``~~~  ',
+   (                        Y
+  {                         I
+ {      -                    `,
+ |       ',                   )
+ |        |   ,..__      __. Y
+ |    .,_./  Y ' / ^Y   J   )|
+ \           |' /   |   |   ||
+  \          L_/    . _ (_,.'(
+   \,   ,      ^^""' / |      )
+     \_  \          /,L]     /
+       '-_~-,       ` `   ./`
+          `'{_            )
+              ^^\..___,.--`
+              """)
+# feel free to remove the skull, i thought it was funny
+
+
+class winScreen(Screen):
+
+    def display(self):
+
+        os.system('cls')
+        print("""
+ __     __          __          ___         _ 
+ \ \   / /          \ \        / (_)       | |
+  \ \_/ /__  _   _   \ \  /\  / / _ _ __   | |
+   \   / _ \| | | |   \ \/  \/ / | | '_ \  | |
+    | | (_) | |_| |    \  /\  /  | | | | | |_|
+    |_|\___/ \__,_|     \/  \/   |_|_| |_| (_)
+              """)
+
+        print("""
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠔⣋⣉⡙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠮⡞⠁⠀⠈⢢⠷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⢠⢤⣇⠀⡇⠀⠀⠀⢸⠀⣸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⡏⢰⠙⠚⢧⣀⢀⣠⠞⠓⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⡸⠀⡎⠀⣀⡤⠏⠉⠧⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⢠⠃⢰⡵⠊⠁⠀⠀⠀⠀⠀⠈⠳⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⢸⡀⠀⣀⡠⡆⠀⠀⠀⠀⠀⣆⠀⠹⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁⠀⡇⠀⠀⠀⠀⠀⡏⢣⡀⠘⣄⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⢸⠀⠙⢤⡈⢦⡀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⢠⠖⣒⣶⠖⠒⠒⠒⠲⠷⣒⠒⠒⠒⠒⣺⣶⠖⠒⠓⢤⣹⠶⣒⠲⡄⠀⠀
+    ⠀⢠⠏⣞⣟⠉⠀⣖⠒⣲⠀⠀⠈⣳⠀⠀⡎⡞⠉⠀⣖⢒⣢⠀⠀⠈⡇⠹⡄⠀
+    ⢠⠏⠀⠘⠪⢅⣀⣀⠉⣀⣀⡠⠔⠁⠀⠀⠙⠮⣇⣀⣀⠉⣀⣀⡤⠖⠁⠀⠹⡄
+    ⡟⠒⠒⠒⠒⠒⠒⠓⠛⠚⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠚⠛⠛⠒⠒⠒⠒⠒⠒⢻
+    ⣇⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣸
+              """)
+
+    # def appendLeaderboard(self, name, score):
+    #     new_entry = {
+    #         "name": name,
+    #         "score": score
+    #     }
+    #     file = open('assets/leaderboard.json', "r")
+    #     data = json.load(file)
+    #     file.close()
+    #     file = open('assets/leaderboard.json', "w")
+    #     data["leaderboard"].append(new_entry)
+    #     json.dump(data, file, indent=4)
+    #     file.close()
+
+    def appendLeaderboard(self, name, score):
+        new_entry = {
+            "name": name,
+            "score": score
+        }
+
+        try:
+            with open('assets/leaderboard.json', "r") as file:
+                data = json.load(file)
+
+            data["leaderboard"].append(new_entry)
+
+            with open('assets/leaderboard.json', "w") as file:
+                json.dump(data, file, indent=4)
+
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
