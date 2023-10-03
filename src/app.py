ef1891bea9fd02d24ac704ab4163af955a0d35f0
Nathan Pike <hikupzz@gmail.com>
2023-10-03 10:44:05 +0100

    bug - tries not being return

--- src/app.py
+++ src/app.py
@@ -3,10 +3,20 @@ import startAgain
 
 
 def main():
-    
-    questionNum = askQuestion.askQuestion()
-    answer = askQuestion.getAnswer()
-    askQuestion.validateAnswer(questionNum, answer)
+    correctTry = 0
+    incorrectTry = 0
+
+    while correctTry <= 3 and incorrectTry <= 3:
+        questionNum = askQuestion.askQuestion()
+        answer = askQuestion.getAnswer()
+        askQuestion.validateAnswer(questionNum, answer, correctTry, incorrectTry)
+        print(correctTry, incorrectTry)
+
+    if correctTry == 3:
+        print("win")
+    elif incorrectTry == 3:
+        print("lose")
+
     if startAgain.startAgain() == 1:
         main()
     else:
