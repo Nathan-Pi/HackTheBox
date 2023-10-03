f145ba227771eacebfa83db7729385ea03538b5c
Ethan McIntosh <128169416+emcint@users.noreply.github.com>
2023-10-03 11:53:44 +0100

    asciiiii

--- src/startScreen.py
+++ src/startScreen.py
@@ -3,7 +3,28 @@ def startScreen():
     entries = 10
     file = open('assets/leaderboard.json')
     data = json.load(file)
-    print("Welcome to Hack The Safe! - a Terrifying Net Force production \n")
+    print('''                                                                                                                                     
+    __ __  ____    __ __  _      ______ __ __   ___       _____ ____ _____  ___ 
+    |  |  |/    |  /  ]  |/ ]    |      |  |  | /  _]     / ___//    |     |/  _]
+    |  |  |  o  | /  /|  ' /     |      |  |  |/  [_     (   \_|  o  |   __/  [_ 
+    |  _  |     |/  / |    \     |_|  |_|  _  |    _]     \__  |     |  |_|    _]
+    |  |  |  _  /   \_|     \      |  | |  |  |   [_      /  \ |  _  |   _]   [_ 
+    |  |  |  |  \     |  .  |      |  | |  |  |     |     \    |  |  |  | |     |
+    |__|__|__|__|\____|__|\_|      |__| |__|__|_____|      \___|__|__|__| |_____|  
+        ''')
+   #print("Welcome to Hack The Safe! - a Terrifying Net Force production \n")
+    print("""
+                                _____________
+                                ||           ||
+                                ||           ||
+                                ||           ||
+                                ||___________||
+                                |   _______   |
+                                |  |    __ |  |
+                                |   |__||  |
+                                '--|_______|--'
+                                
+        """)
     for i in range(entries):
         try:
             print(
