d2449e3c719cb830f77d44e881eff52ee3ac9b0d
Ethan McIntosh <128169416+emcint@users.noreply.github.com>
2023-10-04 10:09:12 +0100

    partial integration of screen classes into main()

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 import askQuestion
-import startAgain
 import screens
 import os
+
 
 def main():
     os.system('cls')
@@ -11,13 +11,12 @@ def main():
     correctTry = 0
     incorrectTry = 0
     accessedNumbers = []
-
+    score = 0
 
     numOfDigits = askQuestion.difficultyChoice()
     numOfDigits += 2
 
     code = askQuestion.generateCode(numOfDigits)
-
 
     while correctTry < numOfDigits and incorrectTry < 4:
         questionNum = askQuestion.askQuestion(accessedNumbers)
@@ -30,14 +29,20 @@ def main():
         os.system('cls')
 
     if correctTry == numOfDigits:
-        askQuestion.codeEntry(code)
+        success = askQuestion.codeEntry(code)
+        if success is True:
+            winScreen = screens.winScreen()
+            winScreen.display()
+            winScreen.appendLeaderboard(username, score)
+            winScreen.restartOrQuit()
+            main()
+
     elif incorrectTry == 4:
         print("The safe turned on its anti-bruteforce defense! You have to try hack the safe again!")
-
-    if startAgain.startAgain() == 1:
+        loseScreen = screens.loseScreen()
+        loseScreen.display()
+        loseScreen.restartOrQuit()
         main()
-    else:
-        quit()
 
 
 main()
